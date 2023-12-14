08a807cf44373888910c49f4adeaf6661ee81118
meeranmohideen.k <startechnology282@gmail.com>
2023-12-14 12:10:00 +0530

    changed

--- Prodapt.CSharp.Day5/Program.cs
+++ Prodapt.CSharp.Day5/Program.cs
@@ -12,6 +12,26 @@
             Console.WriteLine("virat");
             Console.WriteLine("sachin");
 
+            string goal = "hello";
+            Console.WriteLine(goal.ToUpper());
+
+            string stick = " HELLO";
+            Console.WriteLine(stick.ToLower());
+
+            string user = "";
+            Console.WriteLine(string.IsNullOrEmpty(user));
+
+            string pwd = "";
+            Console.WriteLine(string.IsNullOrWhiteSpace(pwd));
+
+            string super = "set123";
+            Console.WriteLine(super.Substring(1, 4));
+
+
+            Console.WriteLine(stick.Trim());
+
+
+
         }
     }
 }
